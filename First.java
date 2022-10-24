17e5fcb147e3ce65520c09c43dbd0be26a9291f7
Tema <tema200133@gmail.cpm>
2022-10-24 23:35:17 +0300

    Second commit

--- First.java
+++ First.java
@@ -1,4 +1,14 @@
 public class First {
     private int a;
     private int b;
+
+    private int c;
+    private int d;
+
+    public First(int a, int b, int c, int d) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+        this.d = d;
+    }
 }
